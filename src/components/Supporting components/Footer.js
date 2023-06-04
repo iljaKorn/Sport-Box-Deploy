8cb2df3f4222f9ef376d6e9d85824ff38ea775b0
iljaKorn <kornilov.ilja@rambler.ru>
2023-06-04 23:00:02 +0300

    Check yml 18

--- src/components/Supporting components/Footer.js
+++ src/components/Supporting components/Footer.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classes from "../../css/main_page.module.css";
+import { getRemoteConfig, getValue, fetchAndActivate } from "firebase/remote-config";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-import classes from '../../css/main_page.module.css';
+function Footer() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBim417nB_Wg8297p3cWnZr0ozRleb83ho",
+        authDomain: "sportequipmentstatistics.firebaseapp.com",
+        projectId: "sportequipmentstatistics",
+        storageBucket: "sportequipmentstatistics.appspot.com",
+        messagingSenderId: "582427848609",
+        appId: "1:582427848609:web:8a4e261bd125dbc700e5e0",
+        measurementId: "G-EDC7KMW5BP"
+        // ... your Firebase config ...
+    };
 
-function Footer(props) {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+
+    const remoteConfig = getRemoteConfig(app);
+    console.log(remoteConfig);
+    const [actualAddress, setActualAddress] = useState("-");
+    const [actualTelephoneNumber, setActualTelephoneNumber] = useState("+");
+
+    useEffect(() => {
+        const fetchRemoteConfig = async () => {
+            await fetchAndActivate(remoteConfig);
+
+            
+            const addressValue = getValue(remoteConfig, "actualAddress");
+            const telephoneNumberValue = getValue(remoteConfig, "actualTelephoneNumber");
+            console.log(addressValue);
+            console.log(telephoneNumberValue);
+
+            setActualAddress(addressValue.asString());
+            setActualTelephoneNumber(telephoneNumberValue.asString());
+        };
+
+        fetchRemoteConfig().catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
     return (
         <footer className={classes.footer}>
             <div className={classes.contacts}>
@@ -10,11 +52,11 @@ function Footer(props) {
                     КОНТАКТНАЯ ИНФОРМАЦИЯ:
                     <br />
                     <br />
-                    394018, Россия, г. Воронеж,
+                    Россия, г. Воронеж,
                     <br />
-                    Университетская площадь, 1
+                    {actualAddress}
                     <br />
-                    +7 (495) 900-56-56
+                    {actualTelephoneNumber}
                     <br />
                     bkatya5577@gmail.com
                     <br />
